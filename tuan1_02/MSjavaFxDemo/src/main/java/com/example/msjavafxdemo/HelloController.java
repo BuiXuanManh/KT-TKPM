5360b10fcbcaa745df4a5d527121e27585e24c44
BuiXuanManh <91316469+BuiXuanManh@users.noreply.github.com>
2023-06-17 18:35:22 +0700

    add minh chung tren lop tuan1_02

--- tuan1_02/MSjavaFxDemo/src/main/java/com/example/msjavafxdemo/HelloController.java
+++ tuan1_02/MSjavaFxDemo/src/main/java/com/example/msjavafxdemo/HelloController.java
@@ -1,14 +1,31 @@
 package com.example.msjavafxdemo;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button bntluu;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText( "Welcome to JavaFX Application!" );
-    }
+    private CheckBox cbcmnd;
+
+    @FXML
+    private CheckBox cbmsbn;
+
+    @FXML
+    private TextField txtCmnd;
+
+    @FXML
+    private AnchorPane txtDiachi;
+
+    @FXML
+    private TextField txtHoten;
+
+    @FXML
+    private TextField txtMsbn;
 }
