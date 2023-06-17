0ce0b75c97ecaf881b5749aff27577204fcef0d3
BuiXuanManh <91316469+BuiXuanManh@users.noreply.github.com>
2023-06-17 16:11:58 +0700

    add file tuan1_02 MSjavaFxDemo

--- tuan1_02/MSjavaFxDemo/src/main/java/com/example/msjavafxdemo/HelloApplication.java
+++ tuan1_02/MSjavaFxDemo/src/main/java/com/example/msjavafxdemo/HelloApplication.java
@@ -1,4 +1,4 @@
-package com.example.javafxdemo;
+package com.example.msjavafxdemo;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,10 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        FXMLLoader fxmlLoader = new FXMLLoader( HelloApplication.class.getResource( "hello-view.fxml" ) );
+        Scene scene = new Scene( fxmlLoader.load(), 320, 240 );
+        stage.setTitle( "Hello!" );
+        stage.setScene( scene );
         stage.show();
     }
 
